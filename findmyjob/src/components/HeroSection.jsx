68837d21bfe785d413d06fb31b8d4d787be89294
Thoms-C <61702462+Thoms-C@users.noreply.github.com>
2021-01-14 16:46:55 +0100

    homePage

--- findmyjob/src/components/HeroSection.jsx
+++ findmyjob/src/components/HeroSection.jsx
@@ -1,21 +1,89 @@
 import React from "react";
 import "../App.css";
-import { Button } from "./Button";
 import "./HeroSection.css";
+import styled from "styled-components"
+
+const Page = styled.div`
+  width: 100%;
+  min-height: 70vh;
+
+  #container {
+    color: white;
+    text-transform: uppercase;
+    font-size: 70px;
+    font-weight: bold;
+    text-align:center;
+    width: 100%;
+    bottom: 45%;
+    margin-top: 25vh
+  }
+
+  #flip {
+    height: 80px;
+    overflow:hidden;
+  }
+
+  #flip > div > div {
+    color: #fff;
+    padding: 4px 12px;
+    height: 70px;
+    margin-bottom: 30px;
+    display: inline-block;
+  }
+
+  #flip div:first-child {
+    animation: show 10s linear infinite;
+  }
+
+  @keyframes show {
+    0% {
+      margin-top: -270px;
+    }
+    5% {
+      margin-top: -180px;
+    }
+    33% {
+      margin-top: -180px;
+    }
+    38% {
+      margin-top: -90px;
+    }
+    66% {
+      margin-top: -90px;
+    }
+    71% {
+      margin-top: 0px;
+    }
+    99% {
+      margin-top: 0px;
+    }
+    100% {
+      margin-top: -270px;
+    }
+  }
+`
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <h1>TROUVEZ VOTRE JOB</h1>
-      <p>Qu'est ce que vous attendez ?</p>
+
+      <Page>
+      <div id='container'>
+      TROUVEZ VOTRE
+        <div id ='flip'>
+          <div>
+            <div>JOB</div>
+          </div>
+          <div>
+            <div>ALTERNANCE</div>
+          </div>
+          <div>
+            <div>STAGE</div>
+          </div>
+        </div>
+      </div>
+      </Page>
       <div className="hero-btns">
-        <Button
-          className="btns"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          S'INSCRIRE
-        </Button>
       </div>
     </div>
   );
