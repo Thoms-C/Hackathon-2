27c23b474efc0a4cec152a47c570cbfb27228f3e
AlexisCatrix <alexis.4x.ac@gmail.com>
2021-01-15 10:39:37 +0100

    Create differents sections for graphics and a sidebar to show user's informations

--- findmyjob/src/components/DashBoard.jsx
+++ findmyjob/src/components/DashBoard.jsx
@@ -1,3 +1,35 @@
+import {
+  Graphic,
+  GraphicsContainer,
+  MainContainer,
+  DashboardContainer,
+  Profil,
+  Icon,
+  ProfilInformation,
+  NameUser,
+} from "../styled-components/DashboardStyled";
+
 export default function Dashboard() {
-  return <div></div>;
+  let user = "Jack";
+
+  return (
+    <DashboardContainer>
+      <Profil>
+        <ProfilInformation>
+          <Icon className="fas fa-user-circle" id="avatar" alt="avatar"></Icon>
+          <NameUser>Welcome back, {user} !</NameUser>
+        </ProfilInformation>
+      </Profil>
+      <MainContainer>
+        <GraphicsContainer className="firstSection">
+          <Graphic>Graphic 1</Graphic>
+          <Graphic>Graphic 2</Graphic>
+          <Graphic>Graphic 3</Graphic>
+        </GraphicsContainer>
+        <GraphicsContainer className="secondSection">
+          <Graphic id="longGraphic">Graphic 4</Graphic>
+        </GraphicsContainer>
+      </MainContainer>
+    </DashboardContainer>
+  );
 }
